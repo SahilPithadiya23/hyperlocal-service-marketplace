099590f4b903fb59bdc07b8ebe390efca876273e
SahilPithadiya23 <sahilpithadiya2304@gmail.com>
2025-12-02 20:57:03 +0530

    Added service provider login registration

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.post('/user/register', authController.registerUser);
 router.post('/user/login', authController.loginUser);
+router.post('/sprovider/register', authController.registerProvider);
+router.post('/sprovider/login',authController.loginProvider);
 
 module.exports = router;
